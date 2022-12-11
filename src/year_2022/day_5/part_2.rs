e7e9628bc2f64a0abec546f5c16ec29b4e73e0e3
jel <25802745+jelni@users.noreply.github.com>
2022-12-11 19:12:53 +0100

    reuse code in day 5 2022

--- src/year_2022/day_5/part_2.rs
+++ src/year_2022/day_5/part_2.rs
@@ -1,5 +1,6 @@
 use advent_of_code::Solve;
-use itertools::Itertools;
+
+use super::shared::{parse_instructions, parse_stacks};
 
 pub struct Solution;
 
@@ -10,55 +11,14 @@ impl Solve for Solution {
 
     fn solve(&self, lines: Vec<String>) -> String {
         let mut lines = lines.into_iter();
-        let mut stacks = Vec::new();
+        let mut stacks = parse_stacks(lines.by_ref());
 
-        for line in lines.by_ref() {
-            if line.is_empty() {
-                break;
-            }
-
-            for (i, mut item) in line.chars().chunks(4).into_iter().enumerate() {
-                if i >= stacks.len() {
-                    stacks.push(Vec::new());
-                }
-
-                if item.by_ref().next().unwrap() == '[' {
-                    stacks[i].insert(0, item.by_ref().next().unwrap());
-                }
-            }
-        }
-
-        for line in lines.by_ref() {
-            let mut chars = line.chars();
-
-            let move_amount = chars
-                .by_ref()
-                .skip(5)
-                .take_while(char::is_ascii_digit)
-                .collect::<String>()
-                .parse::<usize>()
-                .unwrap();
-            let move_from = chars
-                .by_ref()
-                .skip(5)
-                .take_while(char::is_ascii_digit)
-                .collect::<String>()
-                .parse::<usize>()
-                .unwrap()
-                - 1;
-            let move_to = chars
-                .by_ref()
-                .skip(3)
-                .collect::<String>()
-                .parse::<usize>()
-                .unwrap()
-                - 1;
-
-            let mut items = Vec::with_capacity(move_amount);
-            for _ in 0..move_amount {
-                items.push(stacks[move_from].pop().unwrap());
-            }
-            stacks[move_to].extend(items.into_iter().rev());
+        for instruction in parse_instructions(lines) {
+            let stack = stacks.get_mut(instruction.move_from).unwrap();
+            let items = stack
+                .drain(stack.len() - instruction.move_amount..)
+                .collect::<Vec<_>>();
+            stacks[instruction.move_to].extend(items);
         }
 
         stacks
---
+++ src/year_2022/day_5/shared.rs
@@ -0,0 +1,66 @@
+use itertools::Itertools;
+
+pub struct Instruction {
+    pub move_amount: usize,
+    pub move_from: usize,
+    pub move_to: usize,
+}
+
+pub fn parse_stacks(mut lines: impl Iterator<Item = String>) -> Vec<Vec<char>> {
+    let mut stacks = Vec::new();
+
+    for line in lines.by_ref() {
+        if line.is_empty() {
+            break;
+        }
+
+        for (i, mut item) in line.chars().chunks(4).into_iter().enumerate() {
+            if i >= stacks.len() {
+                stacks.push(Vec::new());
+            }
+
+            if item.by_ref().next().unwrap() == '[' {
+                stacks[i].insert(0, item.by_ref().next().unwrap());
+            }
+        }
+    }
+
+    stacks
+}
+
+pub fn parse_instructions(
+    lines: impl Iterator<Item = String>,
+) -> impl Iterator<Item = Instruction> {
+    lines.map(|line| {
+        let mut chars = line.chars();
+
+        let move_amount = chars
+            .by_ref()
+            .skip(5)
+            .take_while(char::is_ascii_digit)
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap();
+        let move_from = chars
+            .by_ref()
+            .skip(5)
+            .take_while(char::is_ascii_digit)
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap()
+            - 1;
+        let move_to = chars
+            .by_ref()
+            .skip(3)
+            .collect::<String>()
+            .parse::<usize>()
+            .unwrap()
+            - 1;
+
+        Instruction {
+            move_amount,
+            move_from,
+            move_to,
+        }
+    })
+}
