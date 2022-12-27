db78065370e9d485c2711bc74e70cbdd8caa8817
jel <25802745+jelni@users.noreply.github.com>
2022-12-27 01:20:39 +0100

    optimize day 23 2022

--- src/year_2022/day_23/shared.rs
+++ src/year_2022/day_23/shared.rs
@@ -3,26 +3,26 @@ use std::collections::HashSet;
 pub const DIRECTIONS: [Direction; 4] = [
     Direction {
         delta: (0, -1),
-        checks: [(0, -1), (1, -1), (-1, -1)],
+        checks: [0, 1, 7], // N, NE, NW
     },
     Direction {
         delta: (0, 1),
-        checks: [(0, 1), (1, 1), (-1, 1)],
+        checks: [4, 3, 5], // S, SE, SW
     },
     Direction {
         delta: (-1, 0),
-        checks: [(-1, 0), (-1, -1), (-1, 1)],
+        checks: [6, 7, 5], // W, NW, SW
     },
     Direction {
         delta: (1, 0),
-        checks: [(1, 0), (1, -1), (1, 1)],
+        checks: [2, 1, 3], // E, NE, SE
     },
 ];
 
 #[derive(Clone, Copy)]
 pub struct Direction {
     pub delta: (i32, i32),
-    pub checks: [(i32, i32); 3],
+    pub checks: [usize; 3],
 }
 
 pub fn parse_elves(lines: impl Iterator<Item = String>) -> HashSet<(i32, i32)> {
@@ -44,30 +44,27 @@ fn propose(
     directions: [Direction; 4],
     elves: &HashSet<(i32, i32)>,
 ) -> Option<(i32, i32)> {
-    if [
-        (0, -1),
-        (1, -1),
-        (1, 0),
-        (1, 1),
-        (0, 1),
-        (-1, 1),
-        (-1, 0),
-        (-1, -1),
+    let checks = [
+        (0, -1),  // N
+        (1, -1),  // NE
+        (1, 0),   // E
+        (1, 1),   // SE
+        (0, 1),   // S
+        (-1, 1),  // SW
+        (-1, 0),  // W
+        (-1, -1), // NW
     ]
     .into_iter()
-    .all(|check| !elves.contains(&(elve.0 + check.0, elve.1 + check.1)))
-    {
+    .map(|check| !elves.contains(&(elve.0 + check.0, elve.1 + check.1)))
+    .collect::<Vec<_>>();
+
+    if checks.iter().all(|&check| check) {
         return None;
     }
 
     directions
         .into_iter()
-        .filter(|direction| {
-            direction
-                .checks
-                .into_iter()
-                .all(|check| !elves.contains(&(elve.0 + check.0, elve.1 + check.1)))
-        })
+        .filter(|direction| direction.checks.into_iter().all(|check| checks[check]))
         .map(|direction| (elve.0 + direction.delta.0, elve.1 + direction.delta.1))
         .next()
 }
