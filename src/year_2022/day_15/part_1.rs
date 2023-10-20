91eb7e80e669e55e7ab7ea6ee8048f07b4c65edc
jel <25802745+jelni@users.noreply.github.com>
2023-10-20 02:44:54 +0200

    optimize day 15 part 1 2022

--- src/year_2022/day_15/part_1.rs
+++ src/year_2022/day_15/part_1.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 use advent_of_code::Solve;
 
 use super::shared::parse_sensors;
@@ -14,21 +12,38 @@ impl Solve for Solution {
     fn solve(&self, lines: Vec<String>) -> String {
         let sensors = parse_sensors(lines.into_iter());
 
-        let xs = sensors
+        let mut ranges = sensors
             .into_iter()
             .filter_map(|sensor| {
                 let y_diff = sensor.y.abs_diff(2_000_000);
                 let range_diff =
                     i32::try_from(sensor.range).unwrap() - i32::try_from(y_diff).unwrap() - 1;
                 if range_diff > 0 {
-                    Some((sensor.x - range_diff - 1)..=(sensor.x + range_diff))
+                    Some((sensor.x - range_diff - 1, sensor.x + range_diff))
                 } else {
                     None
                 }
             })
-            .flatten()
-            .collect::<HashSet<_>>();
+            .collect::<Vec<_>>();
 
-        xs.len().to_string()
+        ranges.sort_unstable();
+
+        let mut taken_positions = 0;
+        let mut last_position: Option<i32> = None;
+
+        for range in ranges {
+            let taken_end =
+                last_position.map_or(range.0, |last_position| last_position.max(range.0));
+
+            if range.1 > taken_end {
+                taken_positions += range.1 - taken_end + 1;
+            }
+
+            if last_position.map_or(true, |last_position| range.1 > last_position) {
+                last_position = Some(range.1 + 1);
+            }
+        }
+
+        taken_positions.to_string()
     }
 }
